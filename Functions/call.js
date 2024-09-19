574dee27b92fd31a0bddd7967d7f47ddb8374c12
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2024-09-20 01:45:30 +0530

    call polyfill

---
+++ Functions/call.js
@@ -0,0 +1,8 @@
+Function.prototype.call = function (context, ...args) {
+  if (typeof this !== 'function') {
+    throw new Error('This is not callable.');
+  }
+
+  context.fn = this;
+  context.fn(...args);
+};
